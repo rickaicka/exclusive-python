b15b25f7304aeff3c37af60518c6836d1d7c90bf
Ricardo Salim <ricardosalim7@hotmail.com>
2025-02-13 00:28:35 -0300

    category and product models/routes

--- exclusive_api/admin.py
+++ exclusive_api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from unicodedata import category
+from .models import Category, Product
 
-# Register your models here.
+
+class Categories(admin.ModelAdmin):
+    list_display = ('id','name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'products',)
+    list_per_page = 20
+admin.site.register(Category, Categories)
+
+class Products(admin.ModelAdmin):
+    list_display = ('id','name', 'description', 'price', 'get_category')
+    def get_category(self, obj):
+        return ", ".join([category.name for category in obj.categories.all()])
+    get_category.short_description = 'Categorias'
+    get_category.admin_order_field = 'categories'
+    list_display_links = ('id', 'name', )
+    search_fields = ('name', 'description', 'price')
+    list_per_page = 20
+admin.site.register(Product, Products)
--- exclusive_api/models.py
+++ exclusive_api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Categoria')
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Nome')
+    description = models.TextField(verbose_name='Descrição')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Preço')
+    categories = models.ManyToManyField('Category', related_name='produtos', verbose_name='Categorias')
+    class Meta:
+        ordering = ["name",]
+
+    def __str__(self):
+        return self.name
