e711d176bf45d9df45b04bc1ffa69c63514ce701
Ricardo Salim <ricardosalim7@hotmail.com>
2025-03-05 00:59:19 -0300

    push pythonanywhere2

--- setup/views.py
+++ setup/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-
+from gitpy import git
 @csrf_exempt
 def update(request):
     if request.method == "POST":
-        repo = git.Repo("https://ricardosalimd.pythonanywhere.com/")
+        repo = git.Repo("ricardosalimd.pythonanywhere.com/")
         origin = repo.remotes.origin
         origin.pull()
         return HttpResponse("Updated code on PythonAnywhere")
