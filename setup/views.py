247ad4ef0688502b79a3681e4025bfbcd93c3191
Ricardo Salim <ricardosalim7@hotmail.com>
2025-03-05 00:56:54 -0300

    push pythonanywhere2

--- setup/views.py
+++ setup/views.py
@@ -5,11 +5,6 @@ from gitpy import git
 @csrf_exempt
 def update(request):
     if request.method == "POST":
-        '''
-        pass the path of the diectory where your project will be 
-        stored on PythonAnywhere in the git.Repo() as parameter.
-        Here the name of my directory is "test.pythonanywhere.com"
-        '''
         repo = git.Repo("https://ricardosalimd.pythonanywhere.com/")
         origin = repo.remotes.origin
         origin.pull()
