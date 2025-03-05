4cd35b0c6ddccab25d1ac5152aebb4f92722dbc1
Ricardo Salim <ricardosalim7@hotmail.com>
2025-03-05 02:50:52 -0300

    merge wishlist

--- exclusive_api/views.py
+++ exclusive_api/views.py
@@ -8,6 +8,7 @@ from .models import Category, Product, User, PaymentInfo, Image, WishList
 from .serializers import CategorySerializer, ProductSerializer, UserSerializer, PaymentInfoSerializer, ImageSerializer, WishListSerializer
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
+from git import Repo
 
 @csrf_exempt
 def update(request):
@@ -17,7 +18,7 @@ def update(request):
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        repo = git.Repo("https://ricardosalimd.pythonanywhere.com/")
+        repo = Repo("https://ricardosalimd.pythonanywhere.com/")
         origin = repo.remotes.origin
         origin.pull()
         return HttpResponse("Updated code on PythonAnywhere")
