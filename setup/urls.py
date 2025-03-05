4162a6000d695b87ff4bd13f8c008c95fa5ba293
Ricardo Salim <ricardosalim7@hotmail.com>
2025-03-05 02:22:34 -0300

    merge wishlist

--- setup/urls.py
+++ setup/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 from exclusive_api.views import (
     CategoryViewSet, ProductViewSet, UserViewSet, PaymentInfoViewSet, WishListViewSet,
-    PaymentInfoApi, ImageViewSet, ProductApi, WishListApi
+    PaymentInfoApi, ImageViewSet, ProductApi, WishListApi,
+    update
 )
 
 router = routers.DefaultRouter()
@@ -16,12 +17,13 @@ router.register('users', UserViewSet, basename='User')
 router.register('images', ImageViewSet, basename='Image')
 #router.register('payment_infos/', PaymentInfoViewSet, basename='PaymentInfo')
 
-router.register('wish_list/', WishListViewSet, basename='WishList')
+router.register('wish_list', WishListViewSet, basename='WishList')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('payment-infos/', PaymentInfoApi.as_view(), name='payment-infos'),
     path('products/', ProductApi.as_view(), name='products'),
     path('wish-list/', WishListApi.as_view(), name='wish-list'),
+    path("update_server/", views.update, name="update"),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
