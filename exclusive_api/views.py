c0321927b1b5a41fbd5af97b27d4d1ac94d3cd0d
Ricardo Salim <ricardosalim7@hotmail.com>
2025-03-05 03:02:28 -0300

    merge wishlist

--- exclusive_api/views.py
+++ exclusive_api/views.py
@@ -19,7 +19,7 @@ def update(request):
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        repo = Repo("https://ricardosalimd.pythonanywhere.com/")
+        repo = Repo("ricardosalimd.pythonanywhere.com/")
         origin = repo.remotes.origin
         origin.pull()
         return HttpResponse("Updated code on PythonAnywhere")
